8bc96423a5c1e3a52024a70ce803b6c836703d61
Julia Harnisch <julia_zanon@usp.br>
2020-12-13 19:26:00 -0300

    Adicionar Nicholas

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from django.db.models.signals import post_save
 from datetime import date
-from unidecode import unidecode
 import datetime as DT
 
 class Produto(models.Model):
